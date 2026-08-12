77bdbd1b7dc6cecfc625877483ff5d298310ae76
TSISabit <tariksaidulislam@gmail.com>
2026-08-12 15:12:55 +0600

    Fixed Student's Result File

--- Student/Result.java
+++ Student/Result.java
@@ -1,14 +1,20 @@
 package Student;
-
 public class Result {
     private String courseCode;
     private double marks;
     private String grade;
-
-    public Result(String courseCode, double marks, String grade) {
+    public Result(String courseCode, double marks) {
         this.courseCode = courseCode;
         this.marks = marks;
-        this.grade = grade;
+        if (marks >= 80) grade = "A+";
+        else if (marks >= 75) grade = "A";
+        else if (marks >= 70) grade = "A-";
+        else if (marks >= 65) grade = "B+";
+        else if (marks >= 60) grade = "B";
+        else if (marks >= 55) grade = "B-";
+        else if (marks >= 50) grade = "C";
+        else if (marks >= 40) grade = "D";
+        else grade = "F";
     }
 
     public String getCourseCode() {
@@ -24,6 +30,6 @@ public class Result {
     }
 
     public void showResult() {
-        System.out.println( courseCode + " | Marks: " + marks + " | Grade: " + grade);
+        System.out.println(courseCode + " | Marks: " + marks + " | Grade: " + grade);
     }
 }
